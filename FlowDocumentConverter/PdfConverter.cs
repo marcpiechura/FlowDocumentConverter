595cd16b506e5d82ad6724573c9131c54a58e218
silv3rcricl3 <piechura@gmx.net>
2015-05-28 11:23:22 +0200

    Converter added

---
+++ FlowDocumentConverter/PdfConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Windows.Documents;
+using Spire.Pdf;
+
+namespace FlowDocumentConverter
+{
+    public static class PdfConverter
+    {
+        public static byte[] ConvertDoc(FlowDocument document)
+        {
+            var xpsFile = XpsConverter.ConverterDoc(document);
+            var tmpXpsFileName = Path.Combine(Path.GetTempPath(), DateTime.Now.Ticks + ".xps");
+            File.WriteAllBytes(tmpXpsFileName, xpsFile);
+
+            var doc = new PdfDocument();
+            doc.LoadFromXPS(tmpXpsFileName);
+            byte[] pdfFile;
+            using (var stream = new MemoryStream())
+            {
+                doc.SaveToStream(stream);
+                pdfFile = stream.ToArray();
+            }
+
+            File.Delete(tmpXpsFileName);
+            return pdfFile;
+        }
+    }
+}
---
+++ FlowDocumentConverter/XpsConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.IO.Packaging;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using System.Windows.Xps.Serialization;
+
+namespace FlowDocumentConverter
+{
+    public static class XpsConverter
+    {
+        public static byte[] ConverterDoc(FlowDocument document)
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var package = Package.Open(stream, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    using (var xpsDoc = new XpsDocument(package, CompressionOption.Maximum))
+                    {
+                        var rsm = new XpsSerializationManager(new XpsPackagingPolicy(xpsDoc), false);
+                        var paginator = ((IDocumentPaginatorSource)document).DocumentPaginator;
+                        rsm.SaveAsXaml(paginator);
+                        rsm.Commit();
+                    }
+                }
+
+                return stream.ToArray();
+            }
+        }
+    }
+}
